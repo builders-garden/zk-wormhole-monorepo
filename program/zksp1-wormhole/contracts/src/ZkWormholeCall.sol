97d39d72d179678920310d4b9225fef969ce8c54
Fabriziogianni7 <fabrizio@onbeam.com>
2025-03-21 18:26:30 +0100

    first version of contracts

--- program/zksp1-wormhole/contracts/src/ZkWormholeCall.sol
+++ program/zksp1-wormhole/contracts/src/ZkWormholeCall.sol
@@ -1,43 +1,43 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.20;
 
-import {ISP1Verifier} from "@sp1-contracts/ISP1Verifier.sol";
+// import {ISP1Verifier} from "@sp1-contracts/ISP1Verifier.sol";
 
-/// The public values returned by the contract call execution.
-struct ContractPublicValues {
-    bytes32 blockHash;
-    address callerAddress;
-    address contractAddress;
-    bytes contractCalldata;
-    bytes contractOutput;
-}
+// /// The public values returned by the contract call execution.
+// struct ContractPublicValues {
+//     bytes32 blockHash;
+//     address callerAddress;
+//     address contractAddress;
+//     bytes contractCalldata;
+//     bytes contractOutput;
+// }
 
-/// @title SP1 ZkWormholeCall.
-/// @notice This contract implements a simple example of verifying the proof of call to a smart 
-///         contract.
-contract ZkWormholeCall {
-    /// @notice The address of the SP1 verifier contract.
-    address public verifier;
+// /// @title SP1 UniswapCall.
+// /// @notice This contract implements a simple example of verifying the proof of call to a smart 
+// ///         contract.
+// contract UniswapCall {
+//     /// @notice The address of the SP1 verifier contract.
+//     address public verifier;
 
-    /// @notice The verification key for the zkWormholeCall program.
-    bytes32 public zkWormholeCallProgramVKey;
+//     /// @notice The verification key for the uniswapCall program.
+//     bytes32 public uniswapCallProgramVKey;
 
-    constructor(address _verifier, bytes32 _zkWormholeCallProgramVKey) {
-        verifier = _verifier;
-        zkWormholeCallProgramVKey = _zkWormholeCallProgramVKey;
-    }
+//     constructor(address _verifier, bytes32 _uniswapCallProgramVKey) {
+//         verifier = _verifier;
+//         uniswapCallProgramVKey = _uniswapCallProgramVKey;
+//     }
 
-    /// @notice The entrypoint for verifying the proof of a zkWormholeCall number.
-    /// @param _proofBytes The encoded proof.
-    /// @param _publicValues The encoded public values.
-    function verifyZkWormholeCallProof(bytes calldata _publicValues, bytes calldata _proofBytes)
-        public
-        view
-        returns (uint160)
-    {
-        ISP1Verifier(verifier).verifyProof(zkWormholeCallProgramVKey, _publicValues, _proofBytes);
-        ContractPublicValues memory publicValues = abi.decode(_publicValues, (ContractPublicValues));
-        uint160 sqrtPriceX96 = abi.decode(publicValues.contractOutput, (uint160));
-        return sqrtPriceX96;
-    }
-}
+//     /// @notice The entrypoint for verifying the proof of a uniswapCall number.
+//     /// @param _proofBytes The encoded proof.
+//     /// @param _publicValues The encoded public values.
+//     function verifyUniswapCallProof(bytes calldata _publicValues, bytes calldata _proofBytes)
+//         public
+//         view
+//         returns (uint160)
+//     {
+//         ISP1Verifier(verifier).verifyProof(uniswapCallProgramVKey, _publicValues, _proofBytes);
+//         ContractPublicValues memory publicValues = abi.decode(_publicValues, (ContractPublicValues));
+//         uint160 sqrtPriceX96 = abi.decode(publicValues.contractOutput, (uint160));
+//         return sqrtPriceX96;
+//     }
+// }
--- program/zksp1-wormhole/contracts/test/ZkWormholeCall.t.sol
+++ program/zksp1-wormhole/contracts/test/ZkWormholeCall.t.sol
@@ -1,56 +1,56 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.17;
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity ^0.8.20;
 
-import {Test, console} from "forge-std/Test.sol";
-import {stdJson} from "forge-std/StdJson.sol";
-import {SP1VerifierGateway} from "@sp1-contracts/SP1VerifierGateway.sol";
-import {ZkWormholeCall} from "../src/ZkWormholeCall.sol";
-import "forge-std/console.sol";
+// import {Test, console} from "forge-std/Test.sol";
+// import {stdJson} from "forge-std/StdJson.sol";
+// import {SP1VerifierGateway} from "@sp1-contracts/SP1VerifierGateway.sol";
+// import {UniswapCall} from "../src/UniswapCall.sol";
+// import "forge-std/console.sol";
 
-struct SP1ProofFixtureJson {
-    bytes proof;
-    bytes publicValues;
-    bytes32 vkey;
-}
+// struct SP1ProofFixtureJson {
+//     bytes proof;
+//     bytes publicValues;
+//     bytes32 vkey;
+// }
 
-contract ZkWormholeCallTest is Test {
-    using stdJson for string;
+// contract UniswapCallTest is Test {
+//     using stdJson for string;
 
-    address verifier;
-    ZkWormholeCall public zkWormholeCall;
+//     address verifier;
+//     UniswapCall public uniswapCall;
 
-    function loadFixture() public view returns (SP1ProofFixtureJson memory) {
-        string memory root = vm.projectRoot();
-        string memory path = string.concat(root, "/src/fixtures/plonk-fixture.json");
-        string memory json = vm.readFile(path);
-        bytes memory jsonBytes = json.parseRaw(".");
-        return abi.decode(jsonBytes, (SP1ProofFixtureJson));
-    }
+//     function loadFixture() public view returns (SP1ProofFixtureJson memory) {
+//         string memory root = vm.projectRoot();
+//         string memory path = string.concat(root, "/src/fixtures/plonk-fixture.json");
+//         string memory json = vm.readFile(path);
+//         bytes memory jsonBytes = json.parseRaw(".");
+//         return abi.decode(jsonBytes, (SP1ProofFixtureJson));
+//     }
 
-    function setUp() public {
-        SP1ProofFixtureJson memory fixture = loadFixture();
-        verifier = address(new SP1VerifierGateway(address(1)));
-        zkWormholeCall = new ZkWormholeCall(verifier, fixture.vkey);
-    }
+//     function setUp() public {
+//         SP1ProofFixtureJson memory fixture = loadFixture();
+//         verifier = address(new SP1VerifierGateway(address(1)));
+//         uniswapCall = new UniswapCall(verifier, fixture.vkey);
+//     }
 
-    function test_ValidZkWormholeCallProof() public {
-        SP1ProofFixtureJson memory fixture = loadFixture();
+//     function test_ValidUniswapCallProof() public {
+//         SP1ProofFixtureJson memory fixture = loadFixture();
 
-        vm.mockCall(verifier, abi.encodeWithSelector(SP1VerifierGateway.verifyProof.selector), abi.encode(true));
+//         vm.mockCall(verifier, abi.encodeWithSelector(SP1VerifierGateway.verifyProof.selector), abi.encode(true));
 
-        uint160 rate = zkWormholeCall.verifyZkWormholeCallProof(fixture.publicValues, fixture.proof);
+//         uint160 rate = uniswapCall.verifyUniswapCallProof(fixture.publicValues, fixture.proof);
 
-        console.log(rate);
-    }
+//         console.log(rate);
+//     }
 
-    function test_Revert_InvalidZkWormholeCallProof() public {
-        vm.expectRevert();
+//     function test_Revert_InvalidUniswapCallProof() public {
+//         vm.expectRevert();
         
-        SP1ProofFixtureJson memory fixture = loadFixture();
+//         SP1ProofFixtureJson memory fixture = loadFixture();
 
-        // Create a fake proof.
-        bytes memory fakeProof = new bytes(fixture.proof.length);
+//         // Create a fake proof.
+//         bytes memory fakeProof = new bytes(fixture.proof.length);
 
-        zkWormholeCall.verifyZkWormholeCallProof(fixture.publicValues, fakeProof);
-    }
-}
+//         uniswapCall.verifyUniswapCallProof(fixture.publicValues, fakeProof);
+//     }
+// }
